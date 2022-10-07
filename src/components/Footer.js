9cfc2f42a0cce6abcc9c9e86b71233c0a3a2a5b0
hariskurtishi <Hariskay95@gmail.com>
2022-10-07 19:29:38 +0200

    finished the project and made some changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,10 +4,10 @@ import Logo from "../assets/images/Logo-1.png";
 
 const Footer = () => {
   return (
-    <Box mt="80px" bgcolor="#fff3f4">
-      <Stack gap="40px" alignItems="center" px="40px" pt="24px">
-        <img src={Logo} alt="logo" width="200px" height="40px" />
-        <Typography variant="h5" pb="40px" mt="20px">
+    <Box mt="80px" bgcolor="#FAF8FF">
+      <Stack gap="40px" alignItems="center" px="40px" pt="56px">
+        <img src={Logo} alt="logo" width="70px" height="70px" />
+        <Typography variant="h6" pb="40px" mt="15px">
           Made with ❤️ by Haris Kurtishi
         </Typography>
       </Stack>
