db807c9a1f27fc192fd3c8e727eb1af165ccef50
hariskurtishi <Hariskay95@gmail.com>
2022-11-23 01:57:29 +0100

    small changes

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,7 @@ const Footer = () => {
       <Stack gap="40px" alignItems="center" px="40px" pt="56px">
         <img src={Logo} alt="logo" width="70px" height="70px" />
         <Typography variant="h6" pb="40px" mt="15px">
-          Made with ❤️ by
-          <a href="https:/hariskurtishi.com/"  rel="noreferrer" className="">
-            {""}
-            Haris Kurtishi
-          </a>
+          Made with ❤️ by Haris Kurtishi
         </Typography>
       </Stack>
     </Box>
